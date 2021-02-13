db5aef366e5ba69a8609cb4625b7a2f7785edc36
Jiahao XU <Jiahao_XU@outlook.com>
2021-02-13 14:08:46 +1100

    Impl openfile and createfile in utility
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/utility.c
+++ src/utility.c
@@ -1,6 +1,13 @@
+#define _POSIX_C_SOURCE 200809L
+
 #include <stdio.h>
+#include <string.h>
 #include <inttypes.h>
 #include <errno.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include "utility.h"
 
@@ -25,4 +32,81 @@ int strtosize(const char *str, const char* *endptr, int base, size_t *dest)
 
     *dest = result;
     return 0;
+}
+
+int open_autorestart(const char *filename, int flags, mode_t mode)
+{
+    int fd;
+
+    do {
+        fd = open(filename, flags, mode);
+    } while (fd == -1 && errno == EINTR);
+
+    return fd;
+}
+
+void print_flags(int flags)
+{
+    if (flags & O_RDONLY)
+        fputs("O_RDONLY", stderr);
+    else if (flags & O_WRONLY)
+        fputs("O_WRONLY", stderr);
+    else if (flags & O_RDWR)
+        fputs("O_RDWR", stderr);
+
+    static const int flags_arr[] = {
+        O_TRUNC,
+        O_NOFOLLOW,
+        O_NOCTTY,
+        O_APPEND,
+        O_EXCL,
+    };
+    static const size_t flags_cnt = sizeof(flags_arr) / sizeof(int);
+    static const char* const flags_strs[] = {
+        ",O_TRUNC",
+        ",O_NOFOLLOW",
+        ",O_NOCTTY",
+        ",O_APPEND",
+        ",O_EXCL",
+    };
+
+    Static_assert((sizeof(flags_arr) / sizeof(int)) == (sizeof(flags_strs) / sizeof(const char*)));
+
+    for (size_t i = 0; i != flags_cnt; ++i) {
+        if (flags & flags_arr[i])
+            fputs(flags_strs[i], stderr);
+    }
+}
+
+int openfile(const char *filename, int flags)
+{
+    int fd = open_autorestart(filename, flags, 0);
+
+    if (fd == -1) {
+        fprintf(stderr, "Failed to %s %s using flags ", "open", filename);
+        print_flags(flags);
+        fprintf(stderr, " : %s\n", strerror(errno));
+    }
+
+    return fd;
+}
+
+int createfile(const char *filename, int flags, mode_t mode)
+{
+    int fd = open_autorestart(filename, flags | O_CREAT, mode);
+
+    if (fd == -1) {
+        fprintf(stderr, "Failed to %s %s using flags ", "create", filename);
+
+        print_flags(flags);
+
+        if (mode & S_ISUID)
+            fputs(" with mode S_ISUID", stderr);
+        if (mode & S_ISGID)
+            fputs(" with mode S_ISGID", stderr);
+
+        fprintf(stderr, " : %s\n", strerror(errno));
+    }
+
+    return fd;
 }
--- src/utility.h
+++ src/utility.h
@@ -2,6 +2,13 @@
 # define __uring_utils_utility_H__
 
 # include <stddef.h>
+# include <sys/types.h>
+
+# ifdef __cplusplus
+#  define Static_assert(expr) static_assert((expr))
+# else
+#  define Static_assert(expr) _Static_assert((expr), # expr) 
+# endif
 
 /**
  * @return  0 on success,
@@ -10,5 +17,24 @@
  *         -3 if out of range
  */
 int strtosize(const char *str, const char **endptr, int base, size_t *dest);
+
+/**
+ * @param filename the file must exist, or this function will fail.
+ * @param flags can be either O_RDONLY, O_WRONLY or O_RDWR and or-ed with any one of the following:
+ *               - O_TRUNC
+ *               - O_NOFOLLOW
+ *               - O_NOCTTY
+ *               - O_APPEND
+ * @return fd on success, -1 on error and print an err msg to stderr
+ */
+int openfile(const char *filename, int flags);
+
+/**
+ * @param filename the file will be created if not exist.
+ * @param flags In addition to the ones specified in openfile, O_EXCL can also be specified.
+ *
+ * createfile would create a file if not exists.
+ */
+int createfile(const char *filename, int flags, mode_t mode);
 
 #endif
